e5ac5b29e1adf1de292fe15939ba1d6ea4ca466a
Abhi Purohit <60559723+Mincherfinder@users.noreply.github.com>
2020-12-17 15:51:36 -0800

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,12 @@
+class Pig extends BaseClass {
+    constructor(x, y) {
+
+    super(x,y,50,50);      
+    this.image = loadImage("sprites/enemy.png")
+    }
+    
+  };
+
+  //inheritance  - create sub/child classes which have some items same as the base class/parent class
+  //- create once and use it multiple
+  
